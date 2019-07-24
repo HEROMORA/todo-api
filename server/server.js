f1533e292577a18ce3fcab45469a3bf6dd2c772b
Omar Alaa <omar.skillful@gmail.com>
2019-07-24 14:49:42 +0200

    Added GET /todos route

--- server/server.js
+++ server/server.js
@@ -13,6 +13,18 @@ app.get('/', (req, res) => {
     res.send('This is the home page');
 });
 
+app.get('/todos', (req, res) => {
+
+    Todo.find().then((todos) => {
+        res.send({
+            "Todos count": todos.length,
+            todos
+        });
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
@@ -27,6 +39,7 @@ app.post('/todos', (req, res) => {
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
+    console.log('You can access it using http://localhost:3000/');
 })
 
 module.exports = {
