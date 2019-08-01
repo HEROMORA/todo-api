0134d301aeffc7364ddda3f7b8704e2e0a2ff3f0
Omar Alaa <omar.skillful@gmail.com>
2019-08-01 19:00:44 +0200

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,12 +1,28 @@
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-var data = {
-    id: 10,
-    name: 'Omar alaa'
-};
+let password = '123abc!';
+
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log('hash:', hash);
+    })
+});
+
+let hashedPassword = '$2a$10$vqOJ4YF2a6cJlUWYVJafrOkHr5FsqHbo4p5bwFQmmeQ/nxQCUckWK';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+// var data = {
+//     id: 10,
+//     name: 'Omar alaa'
+// };
 
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
-var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+// var decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
+
